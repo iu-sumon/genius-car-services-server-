7a73c3879763605ee8636299553fa7eb78c818f0
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-04-28 12:54:20 +0600

    adding steps

--- index.js
+++ index.js
@@ -1,25 +1,83 @@
+//................................step-1
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
-const res = require('express/lib/response');
+const jwt = require('jsonwebtoken')
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+//............................step-2
 
 require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
 
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//............................step-3
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6ptzk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//...........................step-4
 async function run() {
+
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service')
+        const orderCollection = client.db('geniusCar').collection('order')
+        //jwt api
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken })
+        })
 
-        // all data load on UI
+
+
+        //.........................................CREATE...................//
+
+
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result)
+        })
+
+        //...........................................READ..........................//
+
+
         app.get('/service', async (req, res) => {
 
             const query = {};
@@ -29,7 +87,61 @@ async function run() {
 
         })
 
+
+        //..........................................UPDATE........................//
+
+        app.put('/service/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const updatedService = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+
+            const updatedDoc = {
+                $set: {
+                    name: updatedService.name,
+                    price: updatedService.price,
+
+                }
+            };
+            const result = await serviceCollection.updateOne(filter, options, updatedDoc)
+            res.send(result);
+        })
+
+        //..........................................DELETE....................//
+
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+        //............................order collection  api
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email; // jar jar oder email id onushare tar order dekhabe ei jonno email ta neya hosche
+            if (email === decodedEmail) {
+                const query = { email: email }
+                const cursor = orderCollection.find(query)
+                const orders = await cursor.toArray()
+                res.send(orders)
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+
+        })
+
+        //.................................Order Collection post API...................
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
+        })
+
+
         // single data load on UI
+
         app.get('/service/:id', async (req, res) => {
 
             const id = req.params.id;
@@ -37,23 +149,6 @@ async function run() {
             const service = await serviceCollection.findOne(query);
             res.send(service)
         })
-
-        //data post 
-        app.post('/service', async (req, res) => {
-            const newService = req.body;
-            const result = await serviceCollection.insertOne(newService);
-            res.send(result)
-        })
-
-        //delete single data
-        app.delete('/service/:id',async(req,res)=>{
-            const id=req.params.id;
-            const query={_id:ObjectId(id)};
-            const result=await serviceCollection.deleteOne(query);
-            res.send(result);
-        })
-
-
 
 
 
